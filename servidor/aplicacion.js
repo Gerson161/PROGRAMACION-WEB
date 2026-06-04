79d67855d605a77cd79999445f2a39e8481346b4
Gerson_2007 <gersongames98@gmail.com>
2026-06-04 10:02:48 -0500

    Renombrar carpeta publico a public y actualizar aplicacion.js

--- servidor/aplicacion.js
+++ servidor/aplicacion.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "..", "publico")));
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.use("/autenticacion", rutasAutenticacion);
 app.use("/libros", rutasLibros);
